68e18580ada2ff5f857cb155ba4b5923e9ea0304
Collin F <collin.forrester@gmail.com>
2014-01-20 16:41:31 -0600

    added test with mockData loading and cleanup

--- test/specs.js
+++ test/specs.js
@@ -1,3 +1,4 @@
+var fs = require('fs.extra');
 /**
  * Bootstrap
  */
@@ -22,46 +23,58 @@ before(function(done) {
   
   // TODO: Create the database
   // Database.createDatabase.....
+  if(fs.existsSync('.tmp/disk.db')) {
+    fs.unlinkSync('.tmp/disk.db');
+  }
+  fs.copy('test/mockData.db', '.tmp/disk.db', function(err) {
+    if(err) throw err;
 
-  Sails.lift({
-
-    log: {
-      level: 'error'
-    },
-
-    adapters: {
-      // mongo: {
-      //   module: 'sails-mongo',
-      //   host: 'localhost',
-      //   database: 'test_database',
-      //   user: '',
-      //   pass: ''
-      // }
-    }
-
-  }, function(err, sails) {
-    app = sails;
-    done(err, sails);
-  });
-  
-});
- 
- 
-describe('Basic', function(done) {
-  it("should cause error", function(done) {
-    assert.equal(1, 1, "error");
-    done();
-  });
+    Sails.lift({
+      log: {
+        level: 'error'
+      },
+      adapters: {
+        // mongo: {
+        //   module: 'sails-mongo',
+        //   host: 'localhost',
+        //   database: 'test_database',
+        //   user: '',
+        //   pass: ''
+        // }
+      }
+    }, function(err, sails) {
+      app = sails;
+      // finish loading the mock data
+      done(err, sails);
+    });
+  });  
 });
 
 describe('User', function(done) {
-  it("should be able to create", function(done) {
-    // User.create({name: "hee", email: "a@b.c"}, function(err, user) {
-    //   assert.notEqual(user, undefined);
-    //   done();
-    // });
-    assert.equal(1,1);
-    done();
+  it("should return the already created users", function(done) {
+    request(Sails.express.app)
+      .get('/user')
+      .set('Content-Type', 'application/json')
+      .end(function(err, res) {
+        var users = res.body;
+        assert(users.length === 4);
+        assert(res.statusCode === 200);
+        done();
+      });
+  });
+
+  it('should be able to create a user and return the correct stuff', function(done) {
+    request(Sails.express.app)
+      .post('/user')
+      .send({ email: 'e123@mail.com', password: 'asdf', firstName: 'Ed', lastName: 'Doe' })
+      .set('Content-Type', 'application/json')
+      .end(function(err, res) {
+        assert.equal(res.body.password, undefined, 'Response should not contain the password that they sent');
+        assert.equal(res.body.publicKey, '', 'Response should contain an empty publicKey (until they login)');
+        assert.equal(res.body.oath2token, undefined, 'Response should not contain the token.');
+        assert(res.statusCode === 201);
+        done();
+      });
   });
 });
 
@@ -73,7 +86,7 @@ after(function(done) {
  
   // TODO: Clean up db
   // Database.clean...
-
+  fs.unlinkSync('.tmp/disk.db');
   app.lower(done);
   
  
